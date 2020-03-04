f75d458fd3e1701b85d94d7d77ffb8a1dbfe3e6e
PeterKvayt <peter.kvayt@gmail.com>
2020-03-04 19:45:03 +0300

    Начал реализовывать Mapper

---
+++ HRM/BusinessLogicLayer/Mapper/CompanyMapper.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogicLayer.DataTransferObjects;
+using BusinessLogicLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Mapper
+{
+    class CompanyMapper : GeneralMapper<CompanyDTO>,  IMapper
+    {
+
+    }
+}
---
+++ HRM/BusinessLogicLayer/Mapper/GeneralMapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BusinessLogicLayer.Mapper
+{
+    class GeneralMapper<T>
+    {
+        private static readonly Dictionary<Type, Dictionary<string, PropertyInfo>> _propertiesDictionaries
+            = new Dictionary<Type, Dictionary<string, PropertyInfo>>();
+
+        public virtual T Map<T>(object obj) where T : new()
+        {
+            var objProperties = GetProperties(obj.GetType());
+            var resultProperties = GetProperties(typeof(T));
+            var result = new T();
+            foreach (var resultProperty in resultProperties)
+            {
+                if (objProperties.TryGetValue(resultProperty.Key, out var objProperty))
+                {
+                    resultProperty.Value.SetValue(result, objProperty.GetValue(obj));
+                }
+            }
+            return result;
+        }
+
+        private Dictionary<string, PropertyInfo> GetProperties(Type objType)
+        {
+            if (!_propertiesDictionaries.TryGetValue(objType, out var propertiesInfoDictionary))
+            {
+                var infos = objType.GetProperties();
+
+                propertiesInfoDictionary = new Dictionary<string, PropertyInfo>();
+
+                foreach (var propertyInfo in infos)
+                {
+                    propertiesInfoDictionary.Add(propertyInfo.Name, propertyInfo);
+                }
+
+                _propertiesDictionaries.Add(objType, propertiesInfoDictionary);
+            }
+            return propertiesInfoDictionary;
+        }
+    }
+}
