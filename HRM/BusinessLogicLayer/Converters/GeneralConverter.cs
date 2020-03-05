06c1dcd8aaafa3a76867bde89eb8982e5e902f26
PeterKvayt <peter.kvayt@gmail.com>
2020-03-05 18:43:29 +0300

    Начал делать Converter

---
+++ HRM/BusinessLogicLayer/Converters/GeneralConverter.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogicLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Converters
+{
+    abstract class GeneralConverter<DataTransferObject, BusinessModel>
+    {
+        public virtual BusinessModel Convert(DataTransferObject item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual DataTransferObject Convert(BusinessModel item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HRM/BusinessLogicLayer/Interfaces/IConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    interface IConverter<DataTransferObject, BusinessModel>
+    {
+        BusinessModel Convert(DataTransferObject item);
+
+        DataTransferObject Convert(BusinessModel item);
+    }
+}
