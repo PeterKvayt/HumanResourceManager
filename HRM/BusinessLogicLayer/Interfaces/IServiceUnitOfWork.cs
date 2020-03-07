9ae40615ddd74d8e1a08ed5992ce755dd7aefc1b
PeterKvayt <peter.kvayt@gmail.com>
2020-03-07 21:43:37 +0300

    Сделал UnitOfWork для сервисов

---
+++ HRM/BusinessLogicLayer/Interfaces/IServiceUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogicLayer.DataTransferObjects;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    interface IServiceUnitOfWork
+    {
+        IService<ActivityTypeDTO> AcivityTypeService { get; }
+
+        IService<CompanyDTO> CompanyService { get; }
+
+        IService<EmployeeDTO> EmployeeService { get; }
+
+        IService<LegalFormDTO> LegalFormService { get; }
+
+        IService<PositionDTO> PositionService { get; }
+    }
+}
---
+++ HRM/BusinessLogicLayer/Services/ServiceUnitOfWork.cs
@@ -0,0 +1,84 @@
+﻿using BusinessLogicLayer.DataTransferObjects;
+using BusinessLogicLayer.Interfaces;
+using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Services
+{
+    class ServiceUnitOfWork : IServiceUnitOfWork
+    {
+        private readonly IUnitOfWork _dataBase;
+
+        public ServiceUnitOfWork(IUnitOfWork dataBase)
+        {
+            _dataBase = dataBase;
+        }
+
+        private ActivityTypeService _activityTypeService;
+        public IService<ActivityTypeDTO> AcivityTypeService
+        {
+            get
+            {
+                if (_activityTypeService == null)
+                {
+                    _activityTypeService = new ActivityTypeService(_dataBase);
+                }
+                return _activityTypeService;
+            }
+        }
+
+        private CompanyService _companyService;
+        public IService<CompanyDTO> CompanyService
+        {
+            get
+            {
+                if (_companyService == null)
+                {
+                    _companyService = new CompanyService(_dataBase);
+                }
+                return _companyService;
+            }
+        }
+
+        private EmployeeService _employeeService;
+        public IService<EmployeeDTO> EmployeeService
+        {
+            get
+            {
+                if (_employeeService == null)
+                {
+                    _employeeService = new EmployeeService(_dataBase);
+                }
+                return _employeeService;
+            }
+        }
+
+        private LegalFormService _legalFormService;
+        public IService<LegalFormDTO> LegalFormService
+        {
+            get
+            {
+                if (_legalFormService == null)
+                {
+                    _legalFormService = new LegalFormService(_dataBase);
+                }
+                return _legalFormService;
+            }
+        }
+
+        private PositionService _positionService;
+        public IService<PositionDTO> PositionService
+        {
+            get
+            {
+                if (_positionService == null)
+                {
+                    _positionService = new PositionService(_dataBase);
+                }
+                return _positionService;
+            }
+        }
+    }
+}
