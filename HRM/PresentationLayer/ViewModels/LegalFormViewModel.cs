7d68bd0c4caea27bb44d5576abebc0cf1b3538de
PeterKvayt <peter.kvayt@gmail.com>
2020-03-11 19:25:21 +0300

    Добавил ViewModels

---
+++ HRM/PresentationLayer/ViewModels/LegalFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.ViewModels
+{
+    public class LegalFormViewModel
+    {
+    }
+}
