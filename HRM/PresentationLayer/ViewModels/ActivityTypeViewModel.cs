8d237aeae805ff52642bca09b0537ada43359f2a
PeterKvayt <peter.kvayt@gmail.com>
2020-03-11 19:51:16 +0300

    Реализовал ActivityTypesViewModel

--- HRM/PresentationLayer/ViewModels/ActivityTypeViewModel.cs
+++ HRM/PresentationLayer/ViewModels/ActivityTypeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace PresentationLayer.ViewModels
 {
     public class ActivityTypeViewModel
     {
+        public IEnumerable<ActivityTypeModel> ActivityTypeCollection { get; }
+
+        public ActivityTypeViewModel(IEnumerable<ActivityTypeModel> activityTypeCollection)
+        {
+            ActivityTypeCollection = activityTypeCollection;
+        }
     }
 }
