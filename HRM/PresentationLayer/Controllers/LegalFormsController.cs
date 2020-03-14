f8994df03e8d2ab8b5c66ea9718431b55335a233
PeterKvayt <peter.kvayt@gmail.com>
2020-03-14 20:25:48 +0300

    Сделал CRUD для LegalForm с помощью API

--- HRM/PresentationLayer/Controllers/LegalFormsController.cs
+++ HRM/PresentationLayer/Controllers/LegalFormsController.cs
@@ -2,32 +2,115 @@
 using BusinessLogicLayer.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using PresentationLayer.Models;
+using PresentationLayer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace PresentationLayer.Controllers
 {
     public class LegalFormsController : Controller
     {
-        //private readonly IService<LegalFormDTO> _service;
+        private HttpClient _client;
 
-        //public LegalFormsController(IServiceUnitOfWork service)
-        //{
-        //    _service = service.LegalFormService;
-        //}
+        private const string METHOD_NAME = "LegalForms";
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public LegalFormsController(HttpClient client)
+        {
+            _client = client;
 
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
+            if (_client.BaseAddress == null)
+            {
+                _client.BaseAddress = new Uri("http://localhost:65491/api/");
+                _client.DefaultRequestHeaders.Accept.Clear();
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<LegalFormModel> responseLegalFormCollection = new List<LegalFormModel> { };
+            HttpResponseMessage responseMessage = await _client.GetAsync(METHOD_NAME);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                responseLegalFormCollection = await responseMessage.Content.ReadAsAsync<List<LegalFormModel>>();
+            }
+
+            LegalFormViewModel model = new LegalFormViewModel
+            {
+                LegalFormModelCollection = responseLegalFormCollection
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(LegalFormViewModel model)
+        {
+            LegalFormModel legalForm = model.LegalFormModel;
+
+            HttpResponseMessage responseMessage = await _client.PostAsJsonAsync(METHOD_NAME, legalForm);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return Redirect("/" + METHOD_NAME + "/Index");
+            }
+
+            return Redirect("/" + METHOD_NAME + "/Create");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(uint id)
+        {
+            HttpResponseMessage responseMessage = await _client.GetAsync(METHOD_NAME + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                LegalFormModel legalForm = await responseMessage.Content.ReadAsAsync<LegalFormModel>();
+
+                LegalFormViewModel model = new LegalFormViewModel
+                {
+                    LegalFormModel = legalForm
+                };
+
+                return View(model);
+            }
+
+            return Redirect("/" + METHOD_NAME + "/Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(LegalFormViewModel model)
+        {
+            LegalFormModel position = model.LegalFormModel;
+
+            HttpResponseMessage responseMessage = await _client.PutAsJsonAsync(METHOD_NAME, position);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return Redirect("/" + METHOD_NAME + "/Index");
+            }
+
+            return Redirect("/" + METHOD_NAME + "/Update/" + position.Id.Identificator);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(uint id)
+        {
+            HttpResponseMessage responseMessage = await _client.DeleteAsync(METHOD_NAME + "/" + id);
+
+            return Redirect("/" + METHOD_NAME + "/Index");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- HRM/PresentationLayer/ViewModels/LegalFormViewModel.cs
+++ HRM/PresentationLayer/ViewModels/LegalFormViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,8 @@ namespace PresentationLayer.ViewModels
 {
     public class LegalFormViewModel
     {
+        public LegalFormModel LegalFormModel { get; set; }
+
+        public List<LegalFormModel> LegalFormModelCollection { get; set; }
     }
 }
