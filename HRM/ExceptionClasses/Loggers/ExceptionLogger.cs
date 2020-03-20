5216a4c59e2f20f3a7c6365f8e019706c4292141
PeterKvayt <peter.kvayt@gmail.com>
2020-03-20 14:50:30 +0300

    Провел  рефакторинг ExceptionClasses

--- HRM/ExceptionClasses/Loggers/ExceptionLogger.cs
+++ HRM/ExceptionClasses/Loggers/ExceptionLogger.cs
@@ -7,10 +7,14 @@ namespace ExceptionClasses.Loggers
 {
     public static class ExceptionLogger
     {
-        private static readonly object _locker = new object();
-
+        /// <summary>
+        /// Путь к файлу, в котором хранятся логи
+        /// </summary>
         private static readonly string pathToLogDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
 
+        /// <summary>
+        /// Создает директорию, в которой будут храниться логи
+        /// </summary>
         private static void CreateLogDirectory()
         {
             if ( !Directory.Exists(pathToLogDirectory) )
@@ -19,6 +23,10 @@ namespace ExceptionClasses.Loggers
             }
         }
 
+        /// <summary>
+        /// Создает путь к файлу для хранения лога
+        /// </summary>
+        /// <returns>Путь к файлу хранения лога</returns>
         private static string GetLogFileName()
         {
             CreateLogDirectory();
@@ -26,6 +34,10 @@ namespace ExceptionClasses.Loggers
             return string.Format($"{AppDomain.CurrentDomain.FriendlyName}_{DateTime.Now:dd.MM.yyy}.log");
         }
 
+        /// <summary>
+        /// Возвращает путь к файлу хранения лога
+        /// </summary>
+        /// <returns>Путь к файлу</returns>
         private static string GetPathToLogFile()
         {
             string logFileName = GetLogFileName();
@@ -33,6 +45,10 @@ namespace ExceptionClasses.Loggers
             return Path.Combine(pathToLogDirectory, logFileName);
         }
 
+        /// <summary>
+        /// Логирует сообщение об исключении 
+        /// </summary>
+        /// <param name="exception">Исключение</param>
         public static void Log(Exception exception)
         {
             string logMessage = string.Format($"[{DateTime.Now:dd.MM.yyy HH:mm:ss.fff}] Ошибка в методе [{exception.TargetSite.DeclaringType}.{exception.TargetSite.Name}](): {exception.Message}\r\n");
@@ -40,6 +56,12 @@ namespace ExceptionClasses.Loggers
             WriteToFile(logMessage);
         }
 
+        /// <summary>
+        /// Логирует сообщение об исключении
+        /// </summary>
+        /// <param name="message">Сообщение об исключении</param>
+        /// <param name="exceptedClass">Класс, в котором произошло исключение</param>
+        /// <param name="exceptedMethod">Метод, в котором произошло исключение</param>
         public static void Log(string message, string exceptedClass, string exceptedMethod)
         {
             string logMessage = string.Format($"[{DateTime.Now:dd.MM.yyy HH:mm:ss.fff}] Ошибка в методе [{exceptedClass}.{exceptedMethod}()]: {message}\r\n");
@@ -47,11 +69,17 @@ namespace ExceptionClasses.Loggers
             WriteToFile(logMessage);
         }
 
+        /// <summary>
+        /// Записывает в файл логи
+        /// </summary>
+        /// <param name="logMessage">Сообщение, которое необходимо залогировать</param>
         private static void WriteToFile(string logMessage)
         {
-           string path = GetPathToLogFile();
+            string path = GetPathToLogFile();
 
-            lock (_locker)
+            object locker = new object();
+
+            lock (locker)
             {
                 File.AppendAllText(path, logMessage);
             }
