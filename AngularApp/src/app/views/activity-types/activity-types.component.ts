59da3d5f22121fb816ba3863fd9d20bc510e5b01
PeterKvayt <peter.kvayt@gmail.com>
2020-05-22 18:02:02 +0300

    Починил таймер для notFound и заменил картинки для acivityTypes и notFound

--- AngularApp/src/app/views/activity-types/activity-types.component.ts
+++ AngularApp/src/app/views/activity-types/activity-types.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { HttpService} from '../../services/http.service';
 import { ApiConfig } from 'src/assets/configure/api-config';
 
@@ -17,17 +17,28 @@ interface ActivityType{
   styleUrls: ['./activity-types.component.css'],
   providers: [HttpService]
 })
-export class ActivityTypesComponent implements OnInit {
-
-  types: ActivityType[];
+export class ActivityTypesComponent implements OnInit, DoCheck {
 
   constructor(private httpService: HttpService, private apiConfig: ApiConfig ) {
     this.url = this.apiConfig.url + this.apiConfig.activityTypes;
    }
 
-   url: string;
+  types: ActivityType[] = [];
+
+  url: string;
+
+  wrapperClass = 'hide-img-wrapper';
   ngOnInit(){
-    console.log(this.apiConfig.url + this.apiConfig.activityTypes);
-    this.httpService.get(this.apiConfig.url + this.apiConfig.activityTypes).subscribe((data: ActivityType[]) => this.types = data);
+    console.log('onInit');
+    const url = this.apiConfig.url + this.apiConfig.activityTypes;
+    this.httpService.get(url)
+      .subscribe((data: ActivityType[]) => this.types = data);
+  }
+
+  ngDoCheck(){
+    setTimeout(() => {
+      console.log(this.types.length);
+      this.wrapperClass =  this.types.length === 0 ? 'show-img-wrapper' : 'hide-img-wrapper';
+    }, 250);
   }
 }
--- AngularApp/src/app/views/not-found/not-found.component.ts
+++ AngularApp/src/app/views/not-found/not-found.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
+  private timer: any;
+
   ngOnInit() {
     const delay = 5000;
+    this.timer = setTimeout(() => {
+      const btn = document.getElementById('toHomeBtn');
+      btn.click();
+     }, delay);
+  }
 
-    setTimeout(() => {
-     const btn = document.getElementById('toHomeBtn');
-     btn.click();
-
-    }, delay);
+  ngOnDestroy(){
+    clearTimeout(this.timer);
   }
 
 }
